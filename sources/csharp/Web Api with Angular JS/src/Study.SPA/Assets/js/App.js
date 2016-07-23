59b4d5e7acc973aa92fbd77af00f7a02765b00da
Carreno, Thiago <thiago.carreno@wunderman.com>
2016-07-23 16:11:57 -0300

    Study of Web Api with Angular Js.

---
+++ sources/csharp/Web Api with Angular JS/src/Study.SPA/Assets/js/App.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular.module(
+        'StudySpaApp',
+        ['ngRoute', 'ApiModule']
+    ).config(config);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/Views/Movie/index.html',
+                controller: 'movieController'
+            })
+            .when('/movie', {
+                templateUrl: '/Views/Movie/index.html',
+                controller: 'movieController'
+            })
+            .when('/movie/add', {
+                templateUrl: '/Views/Movie/add.html',
+                controller: 'movieController'
+            })
+            .when('/movie/edit/:id', {
+                templateUrl: '/Views/Movie/edit.html',
+                controller: 'movieController'
+            })
+            .when('/movie/delete/:id', {
+                templateUrl: '/Views/Movie/delete.html',
+                controller: 'movieController'
+            })
+            .otherwise('404', {
+                redirectTo: '/Views/Errors/404.html'
+            });
+
+        $locationProvider.html5Mode(true);
+    }
+})();
