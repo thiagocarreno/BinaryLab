6acfbedfa1baf4023feec17a7993b84231da0f21
Carreno, Thiago <thiago.carreno@wunderman.com>
2016-07-23 17:53:21 -0300

    Add call api test.

---
+++ sources/javascript/callApi/assets/js/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    get = function (url) {
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: 'json',
+            success: function (data, textStatus, xhr) {
+                $("#result").text(JSON.stringify(data));
+            },
+            error: function (xhr, textStatus, error) {
+                $("#result").text(error);                
+            }
+        });
+    }
+
+    $("#callApi").click(function (evt) {
+        evt.preventDefault();
+        var url = $("#apiUrl").val();
+        get(url);
+    });
+});
