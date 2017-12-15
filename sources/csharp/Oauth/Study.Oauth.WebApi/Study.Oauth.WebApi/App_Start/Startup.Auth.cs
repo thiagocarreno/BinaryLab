0cda41cc0ff1284f9ec90ecadd8f30441dd9d581
Thiago Roberto Carreño <tcarreno@micromidia-1.local>
2017-12-15 08:34:43 -0200

    OAuth study.

---
+++ sources/csharp/Oauth/Study.Oauth.WebApi/Study.Oauth.WebApi/App_Start/Startup.Auth.cs
@@ -0,0 +1,11 @@
+﻿using Owin;
+
+namespace Study.Oauth.WebApi
+{
+    public partial class Startup
+    {
+        public static void ConfigureAuth(IAppBuilder app)
+        {
+        }
+    }
+}
---
+++ sources/csharp/Oauth/Study.Oauth.WebApi/Study.Oauth.WebApi/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Study.Oauth.WebApi.Startup))]
+namespace Study.Oauth.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            //SwaggerConfig.Register(httpConfiguration);
+
+            ConfigureAuth(app);
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
